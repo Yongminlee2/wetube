20b7330ed6a5c697593ce0b2d9747b6e4ae0e649
dydals5678 <dydals5678@gmail.com>
2019-03-27 00:31:38 +0900

    3.2 2:16초 시작

--- db.js
+++ db.js
@@ -1,3 +1,27 @@
+//import mongoose from "mongoose";
+const mongoose = require("mongoose");
+import dotenv from "dotenv";
+dotenv.config();
+
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    {
+        useNewUrlParser:true,
+        useFindAndModify: false
+    });
+
+    const db = mongoose.connection;
+    const handleOpen = () => console.log("Connected to DB");
+    const handleError = error => console.log(`Error on DB Connection:${error}`);
+    db.once("open",handleOpen);
+    db.on("error",handleError);
+
+
+//새로운 mongoose는 이런식으로 configuration을 보낼수있다.
+
+//121.66.142.174
+/*
 export const videos = [
     {
         id:324393,
@@ -59,4 +83,4 @@ export const videos = [
             abc: "nico@las.com"
         }
     },
-];
+]; */
--- init.js
+++ init.js
@@ -1,6 +1,9 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
