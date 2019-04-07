f78134b48dbd90307b9534efcec83e7914dd8d7b
dydals5678 <dydals5678@gmail.com>
2019-04-08 01:30:36 +0900

    4강 webpack 완료 5강 프론트 css 작업시작

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
 import "../scss/styles.scss";
+
+const something = async () => {
+    console.log('나는 천재다');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname,"assets","js","main.js");
@@ -12,11 +13,19 @@ const OUTPUT_DIR = path.join(__dirname,"static");
 //__dirname는 파일의 절대경로
 
 const config = {
-    entry : ENTRY_FILE, //entry 는 파일이 들어오는곳
+    entry : ["@babel/polyfill", ENTRY_FILE], //entry 는 파일이 들어오는곳
     mode : MODE,
     module : { //module 이 의미하는건 module를 발견할떄마다 rules 라는 조건을 따르라고한다.
         rules: [//rules는 test의 정규식을 따른걸 만나면 use 를 사용한다.
             //rules는 특정 확장자의 파일을 브라우저가 이해할수있게 변환해준 역활을 한다
+            {
+                test :  /\.(scss)$/,
+                use : [
+                    {
+                        loader : 'babel-loader' //js es6 문법을 구식으로 바꿔줌
+                    }
+                ]
+            },
             {
                 test: /\.(scss)$/,  //확장자가scss 인파일을 찾는다.
                 //정규식 시작은 /\ 로시작하며 마지막엔 $/ 로 끝난다
@@ -28,7 +37,12 @@ const config = {
                         loader: "css-loader" //이걸 사용해야 webpack 가 css를 이해한다
                     },
                     {
-                        loader: "postcss-loader" //CSS의 호환성과 관련된걸 해결해줌 EX) IE 지원문법변환
+                        loader: "postcss-loader", //CSS의 호환성과 관련된걸 해결해줌 EX) IE 지원문법변환
+                        options: {
+                            plugins(){
+                                return [autoprefixer({browsers : "cover 99.5%"})];
+                            }
+                        }
                     },
                     {
                         loader: "sass-loader" //scss or sass 를 일반 css로 바꿔주는 plugin
@@ -42,8 +56,9 @@ const config = {
     },
     output : { //output 은 파일이 나가는곳 이다. output 은 객체여야 한다
         path : OUTPUT_DIR,
-        filename : "[name].[format]" //[name].[format]" 파일의 이름과 파일의 확장자,형식 이다.
-    }
+        filename : "[name].js" //[name].[format]" 파일의 이름과 파일의 확장자,형식 이다.
+    },
+    plugins: [new ExtractCSS("styles.css")] // use: ExtractCSS.extract 로 extract에 저장한 파일을 export 해준다
 }; 
 
 //entry 로 sass or scss 파일을 읽어서 css로변환해주고 그 변환된 파일을 output 으로 파일생성해준다.
